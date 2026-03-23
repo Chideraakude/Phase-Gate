5cc9d91707864d010c5370ea91f86d2137f5aa3e
Chideraakude <akudechidera@gmail.com>
2026-03-23 16:01:04 +0100

    Final commit

--- PYTHON/gate_five/simple_arithmetic_app.py
+++ PYTHON/gate_five/simple_arithmetic_app.py
@@ -1,11 +1,13 @@
 #Psuedo code
 #Step I: Import random
-#Step II: 
+#Step II: Create a loop of numbers between one and ten
+#Step III: Initialize first and second number to a random number between 100 and 200 and 1 and 100 respectively
+#Step IV: initialize your random result to the calculation of te first number minus the second number then print the result
 
 
 import random
 
-#def generate_random_number():
+
 for number in range (1, 11):
     first_number = random.randint(100, 200)
     second_number = random.randint(1,100)
@@ -14,6 +16,7 @@ for number in range (1, 11):
         
     print(str(first_number) + "-" + str(second_number) + " " + "=" + " " + str(random_result))
 
+    attempt = 0
     user_score = 0
     random_result = first_number - second_number
     user_input = 0
@@ -22,11 +25,15 @@ for number in range (1, 11):
         user_input = int (input("Guess the answer: "))
 
         if user_input == random_result:
-            user_score = user_score + 1
+            user_score += 1
             print("Correct Answer " + str(random_result))
             break
         else:
             print("Incorrect Answer!!")
+            user_input = int (input("Guess the answer: "))
+            if user_input == random_result:
+                attempt += 1
+            
 print("Your Total Score Is: " + str(user_score))
 
    
