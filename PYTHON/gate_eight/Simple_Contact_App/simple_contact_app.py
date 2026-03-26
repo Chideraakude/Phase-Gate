5703d1d1fa3f7b58919dca085c9bbac0e57cf141
Chideraakude <akudechidera@gmail.com>
2026-03-26 14:21:27 +0100

    Second Commit

--- PYTHON/gate_eight/Simple_Contact_App/simple_contact_app.py
+++ PYTHON/gate_eight/Simple_Contact_App/simple_contact_app.py
@@ -1,36 +1,56 @@
 
-contact_list = []
-def main_menu():
 
+contact_list = []
+Doe
+def main_menu():
     chidera_collector = input
 
     contact_main_menu = """
-    \n--- Contact App Main Menu ---
+--- Contact App Main Menu ---
 
-    1. To Add Contact
-    2. To Remove Contact
-    3. To Find Contact By Phone Number
-    4. To Find Contact By First Name
-    5. To Find Contact By Last Name
-    6. To Edit and Update Contact
-    7. Exit
-    """
-
-
-while(True):
-    print(contact_main_menu)
-    userChoice = chidera_collector("")
+1. To Add Contact
+2. To Remove Contact
+3. To Find Contact By Phone Number
+4. To Find Contact By First Name
+5. To Find Contact By Last Name
+6. To Edit and Update Contact
+7. Exit
+"""
 
 
-
-    if usersChoice == "1" :
-            add_contact(first_name, last_name, phone_number)
-
-
-main_menu()
+    while True:
+        print(contact_main_menu)
+        usersChoice = chidera_collector("")
 
 
-def add_contact(first_name, last_name, phone_number):
+
+        if usersChoice == "1" :
+            add_contact()
+        elif usersChoice == "2":
+            remove_contact()
+    
+        elif usersChoice == "3" :
+            find_contact_by_phone_number()
+
+        
+        elif usersChoice == "4" :
+
+            findContactByFirstName();
+        
+        elif usersChoice == "5" :
+            findContactByLastName();
+        
+        elif usersChoice == "6":
+            editContact();
+        
+        elif usersChoice == "7":
+
+            break;
+        
+        else:
+            print("Invalid Input!!")
+
+def add_contact():
 
     first_name = input ("Enter your first name: ")
     last_name = input ("Enter your last name: ")
@@ -42,7 +62,40 @@ def add_contact(first_name, last_name, phone_number):
 
     print ("Contact Added Successfully")
 
-add_contact()
+
+
+def remove_contact():
+
+    phone_number = input("Enter a number to delete: ")
+
+    for contact in contact_list:
+        if add_contact.phone_number == phone_number:
+            contact_list.remove((first_name))
+            contact_list.remove((last_name))
+            contact_list.remove((phone_number))
+
+            print("Contact Removed Successfully")
+            return
+
+        else:
+            print("Contact Not Found!!")
+
+
+def find_contact_by_phone_number():
+
+    phone_number = input("Enter a phone number: ")
+
+    for contact in contact_list:
+        if contact["phone"] == phone_number:
+            contact_list.remove(contact)
+
+            print(contact ["first_name"], contact["last_name"], contact["phone_number"])
+            return
+
+main_menu()
+
+
+
 
 
 
